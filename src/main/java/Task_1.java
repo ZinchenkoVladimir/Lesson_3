8bc630509784472a663fc30e3e612b4dd714272e
VolodymyrZinchenko <galdrastav@gmail.com>
2019-09-27 11:56:46 +0300

    1

--- src/main/java/Task_1.java
+++ src/main/java/Task_1.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 
 public class Task_1 {
 
-    private String c1 = "Цикл While:";
-    private String c2 = "Цикл Do While:";
-    private String c3 = "Цикл три:";
-    private String c4 = "Цикл четыре:";
+    private String c1 = "While:";
+    private String c2 = "Do While:";
+    private String c3 = "For:";
+    private String c4 = "четыре:";
+    private String c5 = "Цикл ";
     private String x = "\n";
     private int a = 10;
 
@@ -27,6 +28,10 @@ public class Task_1 {
         return c4;
     }
 
+    public String getC5() {
+        return c5;
+    }
+
     public String getX() {
         return x;
     }
@@ -43,24 +48,29 @@ public class Task_1 {
         String c2 = service_for_cycles.getC2();
         String c3 = service_for_cycles.getC3();
         String c4 = service_for_cycles.getC4();
+        String c5 = service_for_cycles.getC5();
         String x = service_for_cycles.getX();
 
         ArrayList<String> cycle_while = service_for_cycles.getArrayByWhile(a);
-        ArrayList<String> cycle_do_while = service_for_cycles.getArrayByWhile(a);
+        ArrayList<String> cycle_do_while = service_for_cycles.getArrayByDoWhile(a);
+        ArrayList<String> cycle_for = service_for_cycles.getArrayByFor(a);
 
-        System.out.println(c1 + x + cycle_while + x + x + c2 + x + cycle_do_while);
+        System.out.println(
+                c5 + c1 + x + cycle_while + x + x +
+                c5 + c2 + x + cycle_do_while + x + x +
+                c5 + c3 + x + cycle_for);
     }
 
     private ArrayList<String> getArrayByWhile (int a) {
 
         ArrayList<String> cycle = new ArrayList<>();
 
-                while (a <= 20) {
-                cycle.add(a + "");
-                a++;
-                }
+        while (a <= 20) {
+            cycle.add(a + "");
+            a++;
+        }
 
-                return cycle;
+        return cycle;
     }
 
     private ArrayList<String> getArrayByDoWhile (int a) {
@@ -68,7 +78,20 @@ public class Task_1 {
         ArrayList<String> cycle = new ArrayList<>();
 
         do {
-            cycle.add(a + x);
+            cycle.add(a + "");
+            a++;
+        }
+        while (a <= 20);
+
+        return cycle;
+    }
+
+    private ArrayList<String> getArrayByFor (int a) {
+
+        ArrayList<String> cycle = new ArrayList<>();
+
+        do {
+            cycle.add(a + "");
             a++;
         }
         while (a <= 20);
