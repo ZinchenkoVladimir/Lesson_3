7947a2b2d80d09ad82036baa7573d88e4a915e40
VolodymyrZinchenko <galdrastav@gmail.com>
2019-09-30 12:32:32 +0300

    1

---
+++ src/main/java/Task_2.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class Task_2 {
+
+    public static void main (String[] args) {
+
+        int i = 1;
+
+        while (i <= 33) {
+            System.out.println(i*3);
+            i++;
+        }
+    }
+}
