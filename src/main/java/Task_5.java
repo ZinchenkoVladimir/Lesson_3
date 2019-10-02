56b641748ac503a31cd463d76bf2881cc5290eb6
VolodymyrZinchenko <galdrastav@gmail.com>
2019-10-02 13:30:48 +0300

    1

--- src/main/java/Task_5.java
+++ src/main/java/Task_5.java
@@ -4,14 +4,21 @@ import java.util.Scanner;
 
 public class Task_5 {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+
+        System.out.println("Enter four integers separated by a space, then press Enter (all non-digital characters will be erased):");
 
         Scanner scanner = new Scanner(System.in);
 
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
+        String e = scanner.next().replaceAll("[^\\d]", "");
+        String f = scanner.next().replaceAll("[^\\d]", "");
+        String g = scanner.next().replaceAll("[^\\d]", "");
+        String h = scanner.next().replaceAll("[^\\d]", "");
+
+        int a = Integer.parseInt(e.length() == 0 ? e = "0" : e);
+        int b = Integer.parseInt(f.length() == 0 ? f = "0" : f);
+        int c = Integer.parseInt(g.length() == 0 ? g = "0" : g);
+        int d = Integer.parseInt(h.length() == 0 ? h = "0" : h);
 
         System.out.println(min1(a,b,c,d));
     }
