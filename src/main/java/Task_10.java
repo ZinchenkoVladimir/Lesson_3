b1a6009858e2b028eeb5af56edd48fefa4e1de2f
VolodymyrZinchenko <galdrastav@gmail.com>
2019-10-03 12:42:23 +0300

    1

---
+++ src/main/java/Task_10.java
@@ -0,0 +1,19 @@
+package main.java;
+
+public class Task_10 {
+
+    public static void main (String[]args){
+
+        int i = 0;
+        String a = "*        *";
+
+        System.out.println("**********");
+
+        while (i<3){
+            System.out.println(a);
+            i++;
+        }
+
+        System.out.println("**********");
+    }
+}
